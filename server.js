dded5dc978fec29f25c964875e7b7e43e2c9f579
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-05 16:16:38 -0500

    notes

--- server.js
+++ server.js
@@ -197,7 +197,7 @@ app.locals.characters = [
         "avatar": ""
       }
 ]
-//handroll avatars and additional properties (e.g. birthday, fav gifts, family)
+//handroll avatars and additional properties (e.g. birthday, species, fav gifts, family)
 
 app.get('/api/v1/characters', (request, response) => {
     if (!app.locals.characters) {
