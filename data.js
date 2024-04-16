776c50a93e2b8ac55357b4cb707c892c9fc5523c
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-04-15 22:20:03 -0500

    Add birthday values

--- data.js
+++ data.js
@@ -5,244 +5,278 @@ const characters = [
     {
         "id": "1",
         "name": "Abigail",
-        "birthday": '',
+        "birthday": 'Fall 13',
         "hobbies": ["playing flute", "mining", "playing video games"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/88/Abigail.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/88/Abigail.png",
+        "favGifts": []
       },
       {
         "id": "2",
         "name": "Alex",
-        "birthday": '',
+        "birthday": 'Summer 13',
         "hobbies": ["playing football", "working out", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png",
+        "favGifts": []
       },
       {
         "id": "3",
         "name": "Elliott",
-        "birthday": '',
+        "birthday": 'Fall 5',
         "hobbies": ["writing", "reading", "fishing"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/b/bd/Elliott.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/b/bd/Elliott.png",
+        "favGifts": []
       },
       {
         "id": "4",
         "name": "Emily",
-        "birthday": '',
+        "birthday": 'Spring 27',
         "hobbies": ["sewing", "making clothes", "baking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png",
+        "favGifts": []
       },
       {
         "id": "5",
         "name": "Haley",
-        "birthday": '',
+        "birthday": 'Spring 14',
         "hobbies": ["photography", "shopping", "sunbathing"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png",
+        "favGifts": []
       },
       {
         "id": "6",
         "name": "Harvey",
-        "birthday": '',
+        "birthday": 'Winter 14',
         "hobbies": ["medicine", "flying drones", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/95/Harvey.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/95/Harvey.png",
+        "favGifts": []
       },
       {
         "id": "7",
         "name": "Leah",
-        "birthday": '',
+        "birthday": 'Winter 23',
         "hobbies": ["painting", "sculpting", "foraging"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/e/e6/Leah.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/e/e6/Leah.png",
+        "favGifts": []
       },
       {
         "id": "8",
         "name": "Maru",
-        "birthday": '',
+        "birthday": 'Summer 10',
         "hobbies": ["inventing", "building robots", "playing video games"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f8/Maru.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f8/Maru.png",
+        "favGifts": []
       },
       {
         "id": "9",
         "name": "Penny",
-        "birthday": '',
+        "birthday": 'Fall 2',
         "hobbies": ["reading", "helping others", "farming"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/a/ab/Penny.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/a/ab/Penny.png",
+        "favGifts": []
       },
       {
         "id": "10",
         "name": "Sam",
-        "birthday": '',
+        "birthday": 'Summer 17',
         "hobbies": ["playing guitar", "skateboarding", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/94/Sam.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/94/Sam.png",
+        "favGifts": []
       },
       {
         "id": "11",
         "name": "Sebastian",
-        "birthday": '',
+        "birthday": 'Winter 10',
         "hobbies": ["programming", "playing video games", "playing guitar"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/a/a8/Sebastian.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/a/a8/Sebastian.png",
+        "favGifts": []
       },
       {
         "id": "12",
         "name": "Shane",
-        "birthday": '',
+        "birthday": 'Spring 20',
         "hobbies": ["raising chickens", "drinking", "playing video games"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/8b/Shane.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/8b/Shane.png",
+        "favGifts": []
       },
       {
         "id": "13",
         "name": "Caroline",
-        "birthday": '',
+        "birthday": 'Winter 7',
         "hobbies": ["gardening", "cooking", "writing"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/87/Caroline.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/87/Caroline.png",
+        "favGifts": []
       },
       {
         "id": "14",
         "name": "Clint",
-        "birthday": '',
+        "birthday": 'Winter 26',
         "hobbies": ["blacksmithing", "mining", "reading"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/3/31/Clint.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/3/31/Clint.png",
+        "favGifts": []
       },
       {
         "id": "15",
         "name": "Demetrius",
-        "birthday": '',
+        "birthday": 'Summer 19',
         "hobbies": ["science", "gardening", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f9/Demetrius.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f9/Demetrius.png",
+        "favGifts": []
       },
       {
         "id": "16",
         "name": "Dwarf",
-        "birthday": '',
+        "birthday": 'Summer 22',
         "hobbies": ["gemology", "mining", "foraging"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/e/ed/Dwarf.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/e/ed/Dwarf.png",
+        "favGifts": []
       },
       {
         "id": "17",
         "name": "Evelyn",
-        "birthday": '',
+        "birthday": 'Winter 20',
         "hobbies": ["knitting", "baking", "gardening"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/8e/Evelyn.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/8e/Evelyn.png",
+        "favGifts": []
       },
       {
         "id": "18",
         "name": "George",
-        "birthday": '',
+        "birthday": 'Fall 24',
         "hobbies": ["watching TV", "complaining", "reading"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/78/George.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/78/George.png",
+        "favGifts": []
       },
       {
         "id": "19",
         "name": "Gus",
-        "birthday": '',
+        "birthday": 'Summer 8',
         "hobbies": ["cooking", "fishing", "playing darts"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/52/Gus.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/52/Gus.png",
+        "favGifts": []
       },
       {
         "id": "20",
         "name": "Jas",
-        "birthday": '',
+        "birthday": 'Summer 4',
         "hobbies": ["playing with animals", "reading", "drawing"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/55/Jas.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/55/Jas.png",
+        "favGifts": []
       },
       {
         "id": "21",
         "name": "Jodi",
-        "birthday": '',
+        "birthday": 'Fall 11',
         "hobbies": ["cooking", "gardening", "exercising"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/4/41/Jodi.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/4/41/Jodi.png",
+        "favGifts": []
       },
       {
         "id": "22",
         "name": "Kent",
-        "birthday": '',
+        "birthday": 'Spring 4',
         "hobbies": ["playing video games", "hunting", "exercising"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/99/Kent.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/9/99/Kent.png",
+        "favGifts": []
       },
       {
         "id": "23",
         "name": "Krobus",
-        "birthday": '',
+        "birthday": 'Winter 1',
         "hobbies": ["collecting artifacts", "playing chess", "reading"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/71/Krobus.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/71/Krobus.png",
+        "favGifts": []
       },
       {
         "id": "24",
         "name": "Leo",
-        "birthday": '',
+        "birthday": 'Summer 26',
         "hobbies": ["stargazing", "camping", "drawing"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1d/Leo.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1d/Leo.png",
+        "favGifts": []
       },
       {
         "id": "25",
         "name": "Lewis",
-        "birthday": '',
+        "birthday": 'Spring 7',
         "hobbies": ["governing", "history", "geography"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png",
+        "favGifts": []
       },
       {
         "id": "26",
         "name": "Linus",
-        "birthday": '',
+        "birthday": 'Winter 3',
         "hobbies": ["foraging", "camping", "reading"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/3/31/Linus.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/3/31/Linus.png",
+        "favGifts": []
       },
       {
         "id": "27",
         "name": "Marnie",
-        "birthday": '',
+        "birthday": 'Fall 18',
         "hobbies": ["farming", "taking care of animals", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/52/Marnie.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/5/52/Marnie.png",
+        "favGifts": []
       },
       {
         "id": "28",
         "name": "Pam",
-        "birthday": '',
+        "birthday": 'Spring 18',
         "hobbies": ["drinking", "watching TV", "complaining"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/d/da/Pam.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/d/da/Pam.png",
+        "favGifts": []
       },
       {
         "id": "29",
         "name": "Pierre",
-        "birthday": '',
+        "birthday": 'Spring 26',
         "hobbies": ["business", "cooking", "gardening"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/7e/Pierre.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/7/7e/Pierre.png",
+        "favGifts": ['Complete Breakfast', 'Jack Be Nimble, Jack Be Thick', 'Salmon Dinner']
       },
       {
         "id": "30",
         "name": "Robin",
-        "birthday": '',
+        "birthday": 'Fall 21',
         "hobbies": ["carpentry", "gardening", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1b/Robin.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/1/1b/Robin.png",
+        "favGifts": []
       },
       {
         "id": "31",
         "name": "Sandy",
-        "birthday": '',
+        "birthday": 'Fall 15',
         "hobbies": ["exploring", "collecting rare items", "gardening"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/4/4e/Sandy.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/4/4e/Sandy.png",
+        "favGifts": []
       },
       {
         "id": "32",
         "name": "Vincent",
-        "birthday": '',
+        "birthday": 'Spring 10',
         "hobbies": ["playing with friends", "drawing", "exploring"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f1/Vincent.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/f/f1/Vincent.png",
+        "favGifts": []
       },
       {
         "id": "33",
         "name": "Willy",
-        "birthday": '',
+        "birthday": 'Summer 24',
         "hobbies": ["fishing", "exploring", "cooking"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/82/Willy.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/8/82/Willy.png",
+        "favGifts": []
       },
       {
         "id": "34",
         "name": "Wizard",
-        "birthday": '',
+        "birthday": 'Winter 17',
         "hobbies": ["alchemy", "potion brewing", "divination"],
-        "avatar": "https://stardewvalleywiki.com/mediawiki/images/c/c7/Wizard.png"
+        "avatar": "https://stardewvalleywiki.com/mediawiki/images/c/c7/Wizard.png",
+        "favGifts": []
 
       }
       //consider including non-giftable folks - you can't befriend them though so it may not be useful
 ]
-//handroll avatars and additional properties (e.g. fav gifts, family)
+//handroll additional properties (e.g. fav gifts, family)
 
 module.exports = {characters}
